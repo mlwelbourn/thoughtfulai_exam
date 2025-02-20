62542a9ac24cfb7a59a1f4760385db1f0d719131
mlwelbourn <mlwelbourn@gmail.com>
2025-02-20 14:16:05 -0700

    thoughtful exam

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,6 @@
 import pytest
 
+
 @pytest.fixture
 def mock_package_measurements():
     return [
--- tests/test_sort.py
+++ tests/test_sort.py
@@ -1,5 +1,6 @@
 from thoughtful_exam import sort
 
+
 def test_sort(mock_package_measurements):
     for width, height, length, mass, expected in mock_package_measurements:
         assert sort(width, height, length, mass) == expected, f"Failed for ({width}, {height}, {length}, {mass})"
