c966f0f8170a66e89c3973308618cb30b1ffb667
Matt Welbourn <mlwelbourn@gmail.com>
2025-02-20 14:01:44 -0700

    thoughtful exam

---
+++ tests/conftest.py
@@ -0,0 +1,13 @@
+import pytest
+
+@pytest.fixture
+def mock_package_measurements():
+    return [
+        (50, 50, 50, 10, "STANDARD"),
+        (200, 50, 50, 10, "SPECIAL"),
+        (50, 50, 50, 25, "SPECIAL"),
+        (200, 50, 50, 25, "REJECTED"),
+        (150, 10, 10, 10, "SPECIAL"),
+        (100, 100, 50, 30, "SPECIAL"),
+        (200, 200, 200, 5, "SPECIAL"),
+    ]
