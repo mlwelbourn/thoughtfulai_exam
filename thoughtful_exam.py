c5458ffed6cd7aa80598c0622e5c679e789a6e14
mlwelbourn <mlwelbourn@gmail.com>
2025-02-20 14:11:51 -0700

    branches are off

--- thoughtful_exam.py
+++ thoughtful_exam.py
@@ -1,7 +1,6 @@
 
 def sort(width: int, height: int, length: int, mass: int) -> str:
     volume = width * height * length
-
     bulky = volume >= 1000000 or width >= 150 or height >= 150 or length >= 150
     heavy = mass >= 20
 
